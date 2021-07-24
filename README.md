39e37daf23933f635171ef4054b4403b83cf4162
Shane Wade <sbramwade@msn.com>
2021-07-24 19:43:49 -0400

    updated README and loading local storage.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Work Day Scheduler Starter Code
+
+## Description
+
+Create a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+![Calendar Application]( "Calendar Application")
+
+## Deployed Website
+
+<https://shanebramble.github.io/calendar-application/>
+
+### Built With
+
+* HTML
+* CSS
+* JavaScript/JQuery
+
+## Contribution
+
+Made with ❤️ by Shane Bramble-Wade
+
+### ©️2021 Calendar Application, Inc
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,16 @@ $(document).ready(function () {
     // Get current time in hour.
     var currentHour = moment().hour();
 
+    $(".saveBtn").on("click", function () {
+        // Gets the current elements description and time hour.
+        var description = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+
+        // Save values to localStorage
+        localStorage.setItem(time, description);
+    });
+
     // Continously updated the blocks by check the time.
     var timeBlockUpdate = function () {
         $(".time-block").each(function () {
@@ -27,13 +37,20 @@ $(document).ready(function () {
                 $(this).addClass("future");
             }
         });
-
-
     };
 
     timeBlockUpdate();
-    var timeBlockUpdated = setInterval(timeBlockUpdate, 15000);
-
+    var timeBlockUpdated = setInterval(timeBlockUpdate, 30000);
 
     // Load events or tasks from local storage.
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
 });
